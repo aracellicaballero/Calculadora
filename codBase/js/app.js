60a88b47859531462f5adf6ca8e7b7b6e9f32eb4
Sara Caballero C <aracelli.caballero@gmail.com>
2017-07-18 20:54:24 -0600

    Versión final 2.Faltaba el efecto de "click" en los botones.

--- codBase/js/app.js
+++ codBase/js/app.js
@@ -36,90 +36,95 @@ var Calculadora = {
   },
   listenerBotonClick: function(){
     var self = this
-
-    // Botones de números
-    for (var i = 0; i < 10; i++) {
-      document.getElementById(i).onclick = function(e){
-        self.escribirEnPantalla(this.id)
-      }
-    }
-    // boton ON
-    document.getElementById('on').onclick = function(e){
-      tamanoCaja = 8
-      self.limpiarPantalla()
-    }
-    // botón signo
-    document.getElementById('sign').onclick = function(e){
-      // Si el número es 0, no hace nada
-      if (caja.innerHTML == '0'){return}
-
-      // determina si ya hay un signo negativo
-      if(caja.innerHTML.indexOf('-') == -1){
-        caja.innerHTML = '-' + caja.innerHTML     //agrega el signo
-      }else {
-        caja.innerHTML = caja.innerHTML.slice(1)  //quita el signo
-      }
-    }
-    // botón raíz
-    document.getElementById('raiz').onclick = function(e){
-      self.tamanoMaximo()
-      //Si el número es diferente de 0, muestra la raíz cuadrada del número
-      var resultado
-      if ((caja.innerHTML > '0')){
-        resultado = caja.innerHTML = Math.sqrt(caja.innerHTML)
-        caja.innerHTML = resultado.toString().substr(0, tamanoCaja)
-      }else{
-        caja.innerHTML = 'ERROR'
-      }
-    }
-    // boton punto
-    document.getElementById('punto').onclick = function(e){
-      // si hay un punto, no hace nada
-      if(caja.innerHTML.indexOf('.') == -1){
-        caja.innerHTML = caja.innerHTML + '.'
-      }
-    }
-    // boton igual
-    document.getElementById('igual').onclick = function(e){
-      // Toma el valor del sessionStorage
-      if ((sessionStorage.numero1) && (sessionStorage.operacion)){
-        switch (sessionStorage.operacion) {
-          case 'mas':
-            self.sumar()
-            break;
-          case 'menos':
-            self.restar()
-            break;
-          case 'por':
-            self.multiplicar()
-            break;
-          case 'dividido':
-            self.dividir()
-            break;
-          default:
-            break;
+    // Lista de Botones
+    var botonesCalculadora = document.getElementsByClassName('tecla')
+    for (var i = 0; i < 19; i++) {
+      if (botonesCalculadora[i].id != 'mas'){
+        botonesCalculadora[i].onmousedown = function(e){
+          this.style = "width:76px;height:62px;padding:-1px;margin:-1px;"
+        }
+        botonesCalculadora[i].onmouseup = function(e){
+          this.style = "width:77px;height:63px;"
         }
       }
-    }
-    // botones de operaciones - dividir
-    document.getElementById('dividido').onclick = function(){
-      self.guardarValor (caja.innerHTML, this.id)
-      caja.innerHTML = ''
-    }
-    // multiplicar
-    document.getElementById('por').onclick = function(){
-      self.guardarValor (caja.innerHTML, this.id)
-      caja.innerHTML = ''
-    }
-    // restar
-    document.getElementById('menos').onclick = function(){
-      self.guardarValor (caja.innerHTML, this.id)
-      caja.innerHTML = ''
-    }
-    // sumar
-    document.getElementById('mas').onclick = function(){
-      self.guardarValor (caja.innerHTML, this.id)
-      caja.innerHTML = ''
+      switch (botonesCalculadora[i].id) {
+        case 'on':    // boton ON
+          botonesCalculadora[i].onclick = function(e){
+            tamanoCaja = 8
+            self.limpiarPantalla()
+          }
+          break;
+        case 'sign':// botón signo
+          botonesCalculadora[i].onclick = function(e){
+            // Si el número es 0, no hace nada
+            if (caja.innerHTML == '0'){return}
+
+            // determina si ya hay un signo negativo
+            if(caja.innerHTML.indexOf('-') == -1){
+              caja.innerHTML = '-' + caja.innerHTML     //agrega el signo
+            }else {
+              caja.innerHTML = caja.innerHTML.slice(1)  //quita el signo
+            }
+          }
+          break;
+        case 'raiz':// botón raíz
+          botonesCalculadora[i].onclick = function(e){
+            self.tamanoMaximo()
+            //Si el número es diferente de 0, muestra la raíz cuadrada del número
+            var resultado
+            if ((caja.innerHTML > '0')){
+              resultado = caja.innerHTML = Math.sqrt(caja.innerHTML)
+              caja.innerHTML = resultado.toString().substr(0, tamanoCaja)
+            }else{
+              caja.innerHTML = 'ERROR'
+            }
+          }
+          break;
+        case 'punto':// boton punto
+          botonesCalculadora[i].onclick = function(e){
+            // si hay un punto, no hace nada
+            if(caja.innerHTML.indexOf('.') == -1){
+              caja.innerHTML = caja.innerHTML + '.'
+            }
+          }
+          break;
+        case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+          botonesCalculadora[i].onclick = function(e){
+            self.escribirEnPantalla(this.id)
+          }
+          break;
+        case 'igual': // boton igual
+          botonesCalculadora[i].onclick = function(e){
+            // Toma el valor del sessionStorage
+            if ((sessionStorage.numero1) && (sessionStorage.operacion)){
+              switch (sessionStorage.operacion) {
+                case 'mas':
+                  self.sumar()
+                  break;
+                case 'menos':
+                  self.restar()
+                  break;
+                case 'por':
+                  self.multiplicar()
+                  break;
+                case 'dividido':
+                  self.dividir()
+                  break;
+                default:
+                  break;
+              }
+            }
+          }
+          break;
+        case 'dividido': case 'por': case 'menos': case 'mas':// Botónes de operaciones
+          botonesCalculadora[i].onclick = function(){
+            self.guardarValor (caja.innerHTML, this.id)
+            caja.innerHTML = ''
+          }
+          break;
+        default:
+          break;
+      }
     }
   },
   sumar: function(){
